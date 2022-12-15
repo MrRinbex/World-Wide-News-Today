16ff711020a33667d89c4c105e0cc9e36ee57965
MrRinbex <contact@leocheffi.com>
2022-12-15 17:24:01 +0100

    add search page, dynamic page and loading page- impliment Livestamp

---
+++ app/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="animate-pulse font-serif text-lg text-gray-400 text-center p-10">
+      Loading News...
+    </div>
+  );
+};
+
+export default Loading;
